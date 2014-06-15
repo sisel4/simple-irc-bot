de74717cc9ce0390ba04b12c0935dfc7cb4ec154
sisel4 <poczta@lysego.pl>
2014-06-15 16:19:21 +0200

    first commit

---
+++ pierwszy.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python
+import socket
+
+DEBUG=1
+#HOST="chat.freenode.net"
+HOST="irc.pirc.pl"
+#HOST="polska.irc.pl"
+PORT=6667
+NICK='TestowyBot'
+IDENT="bot"
+REALNAME="Bot lysyson"
+#CHANNEL="#testowy"
+CHANNEL="#testowy"
+OWNERS=('sisel4', 'pwg')
+class irc_connection:
+	def __init__(self, host, port, nick, ident, name, channel, debug, owners):
+		self.address_set=0
+		self.host=host
+		self.port=port
+		self.nick=nick
+		self.ident=ident
+		self.name=name
+		self.channel=channel
+		self.connection=socket.socket()
+		self.connection.connect((self.host,self.port))
+		self.debug=debug
+		self.owners=owners
+		self.send("NICK %s" % self.nick)
+		self.send("USER %s %s bla :%s" % (self.ident, self.host, self.name))
+		self.topic=''
+	def send(self, data):
+		if self.debug==1:
+			print(">>",data)
+		data=data+'\r\n'
+		data=data.encode('utf-8')
+		self.connection.send(data)
+	def recive(self):
+		buffer=self.connection.recv(2048).decode()
+		buffer=buffer.split('\r\n')
+		return buffer
+	def join(self):
+		self.send("JOIN %s\r\n" % self.channel)
+		self.get_topic()
+	def message(self, data, sender):
+		logfile=open('log','a')
+		sender_nick=sender.split('!')[0]
+		if self.debug==1:
+			print('Got message: %s' % data)
+			print('From: %s' % sender)
+			logfile.write("<%s> %s\n"%(sender_nick, data))
+		else:
+			logfile.write("<%s> %s\n"%(sender_nick, data))
+		if (data.lower().find('xd')!=-1):
+			self.kick(sender_nick, 'iksde')
+		if (data[0]=='!'):
+			if (data=='!hello'):
+				self.send_message_to_channel('hej %s' % sender_nick)
+			if (data=='!op'):
+				if(sender_nick in self.owners):
+					self.op(sender_nick)
+			if (data=='!deop'):
+				if(sender_nick in self.owners):
+					self.deop(sender_nick)	
+
+			if (data.find('!temat')==0):
+				if(sender_nick in self.owners):
+					data=data.replace('!temat ','')
+					self.set_topic(data)
+			if (data.find('!dopisz')==0):
+				if(sender_nick in self.owners):
+					data=data.replace('!dopisz ','')
+					self.get_topic()
+					self.append_topic(data)
+		logfile.close()
+	def send_message_to_channel(self, data):
+		self.send('PRIVMSG %s :%s' % (self.channel, data))
+	def set_mode(self, who, mode):
+		self.send('MODE %s %s' %(who,mode))
+	def op(self,who):
+		self.set_mode(self.channel, '+o %s' % who)
+	def deop(self,who):
+		self.set_mode(self.channel, '-o %s' % who)
+	def kick(self,who,message):
+		if(who!=self.nick):
+			self.send('KICK %s %s %s'% (self.channel, who, message))
+	def set_realserver_address(self, address):
+		self.host=address
+	def get_topic(self):
+		self.send('TOPIC %s' % self.channel)
+		buffer=self.recive()
+		for line in buffer:
+			if len(line)>0:
+				if(':%s 332 %s %s ' % (irc.host, irc.nick, irc.channel) in line):
+					line=line.split(':',2)
+					self.topic=line[2]
+					print('Got Topic: %s' % self.topic)
+			
+	def set_topic(self, topic):
+		self.send('TOPIC %s :%s' % (self.channel, topic))
+	def append_topic(self, topic):
+		self.send('TOPIC %s :%s' % (self.channel, self.topic+' | '+topic))
+buffer=''
+irc=irc_connection(HOST, PORT, NICK, IDENT, REALNAME, CHANNEL, DEBUG, OWNERS)
+while 1:
+	buffer=irc.recive()
+	for line in buffer:
+		if len(line)>0:
+			line=line.rstrip()
+			if(irc.debug==1):			
+				print("<<", end=' ')
+				for element in line:
+					print(element, end="")
+				print("")
+	
+			if(irc.address_set==0):
+				irc.set_realserver_address(line.split(':')[1].split()[0])
+				irc.address_set=1
+				print('Connected to server: %s' % irc.host)
+			if(line[:4]=='PING'):
+				line=line.split()
+				print('Got ping: %s' % line[1])
+				irc.send("PONG %s" % line[1])
+			if('376' in line):
+				irc.join()
+				irc.set_mode(NICK, '+B %s' % NICK)
+			if('PRIVMSG %s' % CHANNEL in line):
+				line=line.split(' PRIVMSG %s :' % irc.channel)
+				sender=line[0]
+				sender=sender[1:]
+				irc.message(line[1] , sender)
+			if('JOIN :%s' % irc.channel in line):
+				line=line.split('JOIN %s' % irc.channel)
+				user=line[0]
+				user=user[1:].split('!')
+				user=user[0]
+				print('%s has joined %s' % (user, irc.channel))
+				irc.send_message_to_channel('Hej %s \o' % user)
+
